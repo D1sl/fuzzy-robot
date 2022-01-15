3172ae4899bf4b162e8cf8b509643545c2a94b6c
D1sl <benjaminmolini@gmail.com>
2022-01-15 10:25:47 -0600

    Adds preliminary logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,13 @@ var ans1Btn = document.querySelector("#ans1");
 var ans2Btn = document.querySelector("#ans2");
 var ans3Btn = document.querySelector("#ans3");
 
+// Current question
+var questionEl = document.querySelector("#question");
+
+// Answer validation
+var ansValidatorEl = document.querySelector("#answervalidation");
+
+
 // Timer
 var timerEl = document.querySelector("#timer");
 
@@ -44,6 +51,28 @@ function countdown() {
         timerEl.textContent = timeLeft;
 
 }
+
+// Question handler
+function questionHandler(i) {
+    var i = 0;
+    if (i < questions.length) {
+        questionEl.textContent = questions[i].question;
+        questionEl.textContent = questions[i].question;
+        ans1Btn.textContent = questions[i].answers[0];
+        ans2Btn.textContent = questions[i].answers[1];
+        ans3Btn.textContent = questions[i].answers[2];
+    }
+}  
+
+// Answer handler
+function answerHandler(event) {
+    event.preventDevault();
+
+
+
+}
+
+questionHandler();
 
 countdown();
 
