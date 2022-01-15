a8ab5e9126b302b2a8f26d954fd19d789959b873
D1sl <benjaminmolini@gmail.com>
2022-01-15 10:09:52 -0600

    Adds questions and queryselectors for page elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,43 @@
+// Countdown timer
 var timeLeft = 60;
-
-var timerEl = document.querySelector("#timer");
-
 var timerId;
 
-timerId = setInterval(countdown, 1000);
+// Page element queryselectors
+// Answer buttons
+var ans1Btn = document.querySelector("#ans1");
+var ans2Btn = document.querySelector("#ans2");
+var ans3Btn = document.querySelector("#ans3");
 
+// Timer
+var timerEl = document.querySelector("#timer");
+
+// Questions
+var questions = [
+    {
+        question: "Question one",
+        answers: ["1. Answer one", "2. Answer two", "3. Answer three"],
+        correctAnswer: "2"
+    },
+    {
+        question: "Question two",
+        answers: ["1. Answer one", "2. Answer two", "3. Answer three"],
+        correctAnswer: "2"
+    },
+    {
+        question: "Question three",
+        answers: ["1. Answer one", "2. Answer two", "3. Answer three"],
+        correctAnswer: "2"
+    },
+    {
+        question: "Question four",
+        answers: ["1. Answer one", "2. Answer two", "3. Answer three"],
+        correctAnswer: "2"
+    },
+] 
+
+
+// MAIN LOGIC
+timerId = setInterval(countdown, 1000);
 function countdown() {
     // Remove a second from the time  
         timeLeft--;
@@ -14,3 +46,4 @@ function countdown() {
 }
 
 countdown();
+
