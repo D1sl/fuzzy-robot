1ab214bee76bfef87824ecc687177d75d3a6542d
D1sl <94911312+D1sl@users.noreply.github.com>
2022-01-16 12:46:21 -0600

    Cleaning the code and adding comments

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -12,7 +12,7 @@ function listScores() {
 
         var scoreRow = document.createElement("li");
         scoreListEl.appendChild(scoreRow);
-        scoreRow.textContent = highscore[i - 1].name + " " + highscore[i - 1].score;
+        scoreRow.textContent = highscore[i - 1].name + ", score: " + highscore[i - 1].score;
         scoreRow.className = "list-group-item d-flex justify-content-left align-items-center";
 
     }
@@ -20,10 +20,10 @@ function listScores() {
 
 // Clear all scores
 clearBtn.addEventListener("click", clearAll);
+
     function clearAll() {
         localStorage.clear();
         location.reload();
     }
-
 
 listScores();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,24 @@
+// Question index
 var i = 0;
 
-// Countdown timer
-var timeLeft = 60;
+var timeLeft = 10;
 var timerId;
 
 var scoreStore = "highscore";
 
 var finalScoreEl = document.querySelector("#finalscore");
-
 var startButton = document.querySelector("#starterbutton")
-
-// Answer buttons
 var ans1Btn = document.querySelector("#ans1");
 var ans2Btn = document.querySelector("#ans2");
 var ans3Btn = document.querySelector("#ans3");
-
 var submitButton = document.querySelector("#submitscore");
-
-// Current question
 var questionEl = document.querySelector("#question");
-
-// Answer validation
 var ansValidatorEl = document.querySelector("#answervalidation");
-
-
-// Timer
 var timerEl = document.querySelector("#timer");
-
 var message = document.querySelector("#answervalidation");
+document.querySelector(".questionnaire").style.display = 'none';
+document.querySelector(".endgame").style.display = 'none';
+document.querySelector(".scoretimer").style.display = 'none';
 
 
 // Questions
@@ -50,39 +41,38 @@ var questions = [
     {
         question: "Question four",
         answers: ["1. Answer one", "2. Answer two", "3. Answer three"],
-        correctAnswer: "1. Answer one2"
+        correctAnswer: "1. Answer one"
     },
 ] 
 
-
 // MAIN LOGIC
+// Timer
 function countdown() {
-    // Remove a second from the time  
+    // Removes a second each time this function runs
         timeLeft--;
         timerEl.textContent = timeLeft;
 
+        // Ends the quiz if the timer runs out
+        if (timeLeft <= 0) {
+            endQuiz();
+        }
 }
 
-
-document.querySelector(".questionnaire").style.display = 'none';
-document.querySelector(".endgame").style.display = 'none';
-document.querySelector(".scoretimer").style.display = 'none';
-
-
+// Event listener on the button to start the quiz
 startButton.addEventListener("click", askQuestions);
 
 // Question iterator
-questionHandler(0);
-
 function askQuestions() {
+    // Displays the timer
     document.querySelector(".scoretimer").style.display = 'inline';
+    // Sets the interval in which countdown(); runs to 1000 milliseconds, or 1 second
     timerId = setInterval(countdown, 1000);
-
-    console.log("Ask questions clicked");
-    
+    // Shows the main quiz UI
     document.querySelector(".questionnaire").style.display = "block";
+    // Hides the start quiz UI
     document.querySelector("#starter").textContent = null;
 
+    // Buttons for answers, and on click runs the checkAnswer function, passing the answer.
     ans1Btn.addEventListener("click", function() {
         checkAnswer(ans1Btn);
     });
@@ -98,7 +88,7 @@ function askQuestions() {
     questionHandler(0);
 }
 
-// Question handler
+// Question UI handler
 function questionHandler(i) {
 
         questionEl.textContent = questions[i].question;
@@ -108,23 +98,23 @@ function questionHandler(i) {
 
 }  
 
-// Answer handler
+// Answer checker
 function checkAnswer(element) {
 
     // Get the current questions index and correct answer and compare it to what the user chose
     correct = questions[i].correctAnswer;
     if (element.textContent === correct) {
-      console.log("Right answer chosen");
       timeLeft = timeLeft + 10;
       answerMessage("Correct!")
     } else {
-        console.log("Wrong answer chosen");
         answerMessage("Incorrect!")
       timeLeft = timeLeft - 10;
     }
 
+    // Move on to next question
     i++;
 
+    // Runs if there are no more questions
     if (i === questions.length) {
       endQuiz();
     } else {
@@ -132,6 +122,7 @@ function checkAnswer(element) {
     }
   }
 
+  // Displays a message to confirm if the answer chosen was correct or incorrect.
   function answerMessage(messageContent) {
       message.textContent = messageContent;
       setTimeout(function() {
@@ -139,23 +130,24 @@ function checkAnswer(element) {
       }, 1500);
   }
 
-
-
+// Ends the quiz
 function endQuiz() {
-
+    // Stops the timer from running
     clearInterval(timerId);
-
+    // Hides the timer
     document.querySelector(".scoretimer").style.display = 'none';
-
-    console.log("Quiz has ended");
+    // Hides the main quiz UI
     document.querySelector(".questionnaire").style.display = 'none';
+    // Shows the endgame UI that gives the player the final score and options to save high score
     document.querySelector(".endgame").style.display = 'block';
+    // Updates final score with what ever was left on the clock
     finalScoreEl.textContent = timeLeft;
 } 
 
 // Store high scores
 submitButton.addEventListener("click", function(event) {
-    debugger;
+    // Prevents the browser from reloading on form submit
+    event.preventDefault();
     var oldScore = JSON.parse(localStorage.getItem(scoreStore));
     if (oldScore === null) {
         oldScore = Array();
@@ -168,6 +160,5 @@ submitButton.addEventListener("click", function(event) {
     };
     oldScore.push(highscore);
     localStorage.setItem(scoreStore, JSON.stringify(oldScore));
+    window.location.href = "scores.html";
 })
-
-
